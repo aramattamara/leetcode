ccebf212071c2f9c12c10fcbd10163bde8488459
Tamara Lazerka <98753789+aramattamara@users.noreply.github.com>
2024-01-16 18:00:13 -0800

    Time: 46 ms (96.92%), Space: 44.1 MB (73.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,19 +3,24 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    const length = nums.length + 1; 
+    const n = nums.length; 
 
     
-    let actualSum = 0;
-    for (let i = 0; i < nums.length; i++){
-        actualSum += nums[i]
-    }
+//     let actualSum = 0;
+//     for (let i = 0; i < nums.length; i++){
+//         actualSum += nums[i]
+//     }
     
-    let expectedSum = 0; 
-    for (let i = 0; i < length; i++){
-        expectedSum += i
-    }
+//     let expectedSum = 0; 
+//     for (let i = 0; i < length; i++){
+//         expectedSum += i
+//     }
+    
+    let expectedSum = (n * (n + 1)) / 2;
+    
+    const actualSum = nums.reduce((acc, current) => acc + current, 0)
 
     let number = expectedSum - actualSum
+    
     return number
 };
