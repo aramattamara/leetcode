3910a95ba0f4837ca63eef36d0ad3eab088ac284
Tamara Lazerka <98753789+aramattamara@users.noreply.github.com>
2024-01-23 15:52:24 -0800

    Time: 72 ms (95.12%), Space: 51.2 MB (5.03%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -3,37 +3,40 @@
  * @param {number[]} s
  * @return {number}
  */
-// var findContentChildren = function(g, s) {
-    
-//     let result = 0;
-    
-//     let sortedS = s.sort((a, b ) => b - a);
-//     let sortedG = g.sort((a, b ) => b - a);
-    
-//     for (let i = 0; i >= g.length; i++){
-//         if (g[i] <= s[s.length - 1]) {  
-//             result ++;
-//             s.pop()
-//         }
-       
-//     }
-//     return result;
-// };
 
+// if sorted from low to high
 var findContentChildren = function(g, s) {
-
+    
     let result = 0;
-    let j = 0;
-
-    let sortedS = s.sort((a, b ) => b - a);
-    let sortedG = g.sort((a, b ) => b - a);
-
-    for (let i = 0; i < g.length; i++){
-        if (g[i] <= s[j]) {
+    
+    let sortedS = s.sort((a, b ) => a - b);
+    let sortedG = g.sort((a, b ) => a - b);
+    
+    for (let i = g.length - 1; i >= 0 ; i--){
+        if (g[i] <= s[s.length - 1]) {  
             result ++;
-            j++;
+            s.pop()
         }
-
+       
     }
     return result;
 };
+
+// if sorted from high to low
+// var findContentChildren = function(g, s) {
+
+//     let result = 0;
+//     let j = 0;
+
+//     let sortedS = s.sort((a, b ) => b - a);
+//     let sortedG = g.sort((a, b ) => b - a);
+
+//     for (let i = 0; i < g.length; i++){
+//         if (g[i] <= s[j]) {
+//             result ++;
+//             j++;
+//         }
+
+//     }
+//     return result;
+// };
