5d8d7e6440fb47aaf26456db68fb9734294a03d1
Tamara Lazerka <98753789+aramattamara@users.noreply.github.com>
2024-01-23 18:01:38 -0800

    Time: 56 ms (42.65%), Space: 48.6 MB (5.75%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,25 +5,42 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+// var merge = function(nums1, m, nums2, n) {
+//     let last = m + n - 1;
+//     let index1 = m - 1;
+//     let index2 = n - 1;
+    
+//     while (index2 >= 0){
+//         if (nums1[index1] > nums2[index2]){
+//             nums1[last] = nums1[index1];
+//             index1--;
+//         } else { 
+//             nums1[last] = nums2[index2]
+//             index2--
+//         }
+//         last--; 
+//     }
+// };
+
 var merge = function(nums1, m, nums2, n) {
     let last = m + n - 1;
-    let index1 = m - 1;
-    let index2 = n - 1;
-    
-    while (index2 >= 0){
-        if (nums1[index1] > nums2[index2]){
-            nums1[last] = nums1[index1];
-            index1--;
-        } else { 
-            nums1[last] = nums2[index2]
-            index2--
+
+    while (m > 0 && n > 0){
+        if (nums1[m - 1] > nums2[n - 1]){
+            nums1[last] = nums1[m - 1];
+            m = m - 1;
+            last = last - 1;
+        } else {
+            nums1[last] = nums2[n - 1];
+            n = n - 1;
+            last = last - 1;
         }
-        last--; 
     }
-    
-    // while (n > 0) {
-    //     nums1[last] = nums2[n - 1];
-    //     n--;
-    //     last = last - 1;
-    // }
+    while (n > 0){
+        nums1[last] = nums2[n - 1];
+        n--;
+        last = last - 1;
+    }
+
+    return nums1;
 };
